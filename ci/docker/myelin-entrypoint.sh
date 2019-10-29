48bb4ddeb5f7223c797941e808376cde58c0fc38
khsibr <ryadh.khsib@gmail.com>
2019-10-29 12:04:18 +0000

    integrated myelin hpo.

--- ci/docker/myelin-entrypoint.sh
+++ ci/docker/myelin-entrypoint.sh
@@ -3,6 +3,8 @@
 set -e
 
 echo Script params: "$@"
+echo "MODEL_PATH=${MODEL_PATH}"
+echo "DATA_PATH=${DATA_PATH}"
 
 echo "$1" > /tmp/ludwig_model_tmpl
 
@@ -12,8 +14,9 @@ sed 's/\[%/{{/g'   /tmp/ludwig_model_tmpl | sed 's/%\]/}}/g' > /tmp/ludwig_model
 
 cat /tmp/ludwig_model
 
+
 mkdir -p  "${MODEL_PATH}"
 
-$2
+ln -s ${DATA_PATH}/$2 ${MODEL_PATH}/$2
 
-$3
+ludwig $3  --data_csv ${MODEL_PATH}/$2 --model_definition_file /tmp/ludwig_model    --output_directory ${MODEL_PATH} --output_directory ${MODEL_PATH} $4
