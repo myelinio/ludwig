f912c750d638ab2f46edfad01ce57095a49c0b9a
khsibr <ryadh.khsib@gmail.com>
2019-10-29 10:03:07 +0000

    integrated myelin hpo.

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ extra_requirements['gpu'] = [tensorflow_gpu]
 setup(
     name='ludwig-myelin',
 
-    version='0.2.1.13',
+    version='0.2.1.14',
 
     description='A deep learning experimentation toolbox',
     long_description=long_description,
