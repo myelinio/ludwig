ef89e3e92707ff4508db7605cd00726ea466f3fb
khsibr <ryadh.khsib@gmail.com>
2019-11-29 15:27:27 +0000

    add horovod.

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ extra_requirements['gpu'] = [tensorflow_gpu]
 setup(
     name='ludwig-myelin',
 
-    version='0.2.1.24',
+    version='0.2.1.25',
 
     description='A deep learning experimentation toolbox',
     long_description=long_description,
